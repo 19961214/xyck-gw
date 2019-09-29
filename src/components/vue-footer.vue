1d481d0b4ccfae2a01ebad2982a4f26e313aba0d
刘禄璐 <12697481982@qq.com>
2019-09-29 16:00:47 +0800

    123

---
+++ src/components/vue-footer.vue
@@ -0,0 +1,107 @@
+<template>
+  <el-row class="footer">
+    <el-col :span="10" :offset="4">
+      <div class="content">
+        <div class="content-top">杭州厦云创科信息技术有限公司</div>
+        <div class="content-bottom">
+          <div class="item-wrap">
+            <img src="../assets/img/icon_phone.png" alt />
+            <div class="text1">0755-8288 5278</div>
+          </div>
+          <div class="item-wrap">
+            <img src="../assets/img/icon_address.png" alt />
+            <div class="text2">深圳市南山区高新园科技南十二路朗诗大厦1209室</div>
+          </div>
+		  <div class="item-wrap"><div class="text3">掌掌财服务平台入口 ></div></div>
+          
+        </div>
+      </div>
+    </el-col>
+    <el-col :span="8" :offset="2">
+      <div class="tel content">
+        <img src="../assets/img/code.png" alt />
+        <div class="img-text">关注掌掌财小程序</div>
+      </div>
+    </el-col>
+    <div class="bottom">2019 杭州厦云创科信息技术有限公司 粤ICP备17126097号-2</div>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: "vue-footer"
+};
+</script>
+<style lang="less">
+
+.footer {
+  background-color: #223a53;
+  height: 360px;
+  color: white;
+  font-size: 14px;
+  // padding: 50px 0 50px 0;
+  position: relative;
+  .content {
+    margin-top: 176px;
+    .content-top {
+      font-size: 20px;
+      color: #fff;
+    }
+    .content-bottom {
+      margin-top: 24px;
+      padding-top: 5px;
+      font-size: 14px;
+      color: #617e9a;
+	  border-top: 1px solid #37516b;
+	  .item-wrap{
+		  display: inline-block;
+		  margin-top: 10px;
+	  }
+		img {
+			display: inline-block;
+			width: 20px;
+			height: 20px;
+			margin-right: 10px;
+		}
+		.text1 {
+			padding-right: 60px;
+			display: inline-block;
+		}
+		.text2 {
+			padding-right: 90px;
+			display: inline-block;
+		}
+		.text3 {
+			color: #ffffff;
+			display: inline-block;
+		}
+    }
+  }
+  .tel {
+    margin-top: 139px;
+    img {
+      padding: 10px 0 0 10px;
+      width: 100px;
+      height: 100px;
+    }
+    .img-text {
+      color: #6986a3;
+      font-size: 14px;
+      padding: 10px 0 0 6px;
+    }
+  }
+}
+.bottom {
+  width: 100%;
+  height: 40px;
+  background-color: #1a334d;
+  font-size: 12px;
+  color: #617e9a;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+}
+</style>
---
+++ src/components/vue-header.vue
@@ -0,0 +1,131 @@
+<template>
+  <el-row>
+    <el-col :span="24" class>
+      <div class="bg-black">
+        <el-row class="nav">
+          <el-col :span="2" :offset="2">
+            <img src="../assets/img/logo.png" alt />
+          </el-col>
+          <router-link to="/index">
+            <el-col :span="2" :offset="1">首页</el-col>
+          </router-link>
+          <router-link to="/product">
+            <el-col :span="2" :offset="0">产品介绍</el-col>
+          </router-link>
+          <router-link to="/team">
+            <el-col :span="2" :offset="0">解决方案</el-col>
+          </router-link>
+          <router-link to="/story">
+            <el-col :span="2" :offset="0">活动咨询</el-col>
+          </router-link>
+          <router-link to="/article">
+            <el-col :span="2" :offset="0">关于厦云</el-col>
+          </router-link>
+          <router-link to="/us">
+            <el-col :span="2" :offset="0">联系我们</el-col>
+          </router-link>
+          <router-link to="/gous">
+            <el-col :span="2" :offset="0">加入我们</el-col>
+          </router-link>
+          <el-col class="phone" :span="4" :offset="1">0755-25888852</el-col>
+        </el-row>
+      </div>
+      <div class="select">
+        <el-select v-model="option" placeholder="请选择" @change="link">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+      </div>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: "vue-header",
+  data() {
+    return {
+      options: [
+        { label: "首页", value: "index" },
+        { label: "产品介绍", value: "product" },
+        { label: "解决方案", value: "team" },
+        { label: "活动咨询", value: "story" },
+        { label: "关于厦云", value: "article" },
+        { label: "联系我们", value: "us" },
+        { label: "加入我们", value: "gous" }
+      ],
+      option: "index"
+    };
+  },
+  methods: {
+    link: function() {
+      this.$router.push({ path: "/" + this.option });
+    }
+  }
+};
+</script>
+
+<style lang="less">
+@import url("../assets/css/reset.css");
+@media (max-width: 768px) {
+  .bg-black {
+    display: none;
+  }
+  .select {
+    display: block !important;
+	position: absolute !important;
+	z-index: 100;
+	width: 100%;
+    top: 0;
+    left: 0;
+  }
+}
+.select {
+  display: none;
+  div {
+    width: 100%;
+  }
+}
+.bg-black {
+  z-index: 1000;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0);
+  width: 100%;
+  height: 60px;
+  line-height: 60px;
+  .nav {
+    font-size: 14px;
+    .router-link-active {
+      color: red;
+    }
+    a {
+      text-decoration: none;
+      color: red;
+      div {
+        cursor: pointer;
+        color: #fff;
+        text-align: center;
+      }
+      div:hover {
+        color: #15abfe;
+      }
+    }
+    .router-link-active {
+      div {
+        border-bottom: 3px solid #15abfe;
+        color: #15abfe;
+      }
+    }
+    .phone {
+      color: #666666;
+      font-size: 14px;
+    }
+  }
+}
+</style>
